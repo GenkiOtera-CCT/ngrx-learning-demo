863fff533945ed8add79017359c07b8b863325af
TechPeriod <77057831+tech-period@users.noreply.github.com>
2024-06-25 23:23:56 +0900

    単純なObservableパターンを実装

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,40 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppService } from './app.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    FormsModule,
+    RouterOutlet,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'ngrx-learning-demo';
+
+  isSubscribing: boolean = false;
+  simpleObserveText: string;
+
+  constructor(
+    // 待機時間を同期するために敢えてpublicにしているので、参考にしないように。
+    public service: AppService
+  ) {
+    this.simpleObserveText = service.initialText;
+  }
+  
+  onClickStartSimpleObserve(): void {
+    this.service.simpleManObservable$.subscribe({
+      next: (value) => {
+        this.isSubscribing = true;
+        this.simpleObserveText = value;
+      },
+      complete: () => {
+        this.isSubscribing = false;
+        this.simpleObserveText = this.service.initialText;
+      }
+    });
+  }
 }
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,28 +1,55 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subscriber } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
 
-  public simpleManObservable$ = new Observable<string>((observer) => {
-    observer.next('1: はじめまして');
-    observer.next('2: (年齢)30歳');
-    observer.next('3: (性別)男性');
-    observer.next('4: (趣味)そば打ち');
-    observer.next('5: 以上です。');
-    observer.complete();
-  });
+  readonly initialText: string = '0: 待機中';
+  waitInterval: number = 1;
 
-  public simpleWomanObservable$ = new Observable<string>((observer) => {
-    observer.next('1: はじめまして');
-    observer.next('2: (年齢)30歳');
-    observer.next('3: (性別)男性');
-    observer.next('4: (趣味)そば打ち');
-    observer.next('5: 以上です。');
-    observer.complete();
+  //#region サンプル用のObservableの定義（ただのObservableは事前にsubscribeされた際の処理を定義する）
+  simpleManObservable$ = new Observable<string>((observer) => {
+    const massages: string[] = [
+      '1: はじめまして',
+      '2: (年齢)30歳',
+      '3: (性別)男性',
+      '4: (趣味)そば打ち',
+      '5: 以上です。',
+    ];
+    
+    // デモ用の非同期処理のため、参考にしないように。
+    (async () => {
+      // 一定間隔でメッセージを更新
+      await this.pushMessagesAsync(observer, massages);
+      // 終了を通知
+      this.toCompleteAsync(observer);
+    })();
   });
+  //#endregion
 
   constructor() { }
+
+  // デモ用に任意の時間間隔でメッセージを送信する非同期処理
+  private async pushMessagesAsync(observer: Subscriber<string>, messages:string[]): Promise<void> {
+    for (const message of messages) {
+      await new Promise<void>((resolve) => {
+        setTimeout(() => {
+          observer.next(message);
+          resolve();
+        }, this.waitInterval * 1000);
+      });
+    }
+  }
+
+  // デモ用に任意の時間後に完了を通知する非同期処理
+  private async toCompleteAsync(observer: Subscriber<string>): Promise<void> {
+    await new Promise<void>((resolve) => {
+      setTimeout(() => {
+        observer.complete();
+        resolve();
+      }, this.waitInterval * 1000);
+    });
+  }
 }
