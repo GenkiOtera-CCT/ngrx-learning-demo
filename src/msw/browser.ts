fdfbd11c5e4d153d1322d4101b275de4030bc9e2
TechPeriod <77057831+tech-period@users.noreply.github.com>
2024-06-28 01:56:19 +0900

    MSWを導入

---
+++ src/msw/browser.ts
@@ -0,0 +1,35 @@
+import { http, HttpResponse, delay } from 'msw';
+import { setupWorker } from 'msw/browser';
+
+const apiBaseUrl = '/api';
+
+export const worker = setupWorker(
+    http.get(`${apiBaseUrl}/short`, () => {
+        return HttpResponse.json({ message: 'Short time request' });
+    }),
+
+    http.get(`${apiBaseUrl}/long`, async () => {
+        await delay(5000);
+        return HttpResponse.json({ message: 'Long time request' });
+    }),
+
+    http.get(`${apiBaseUrl}/error`, () => {
+        return new HttpResponse(null, {
+            status: 500,
+            statusText: 'Internal Server Error',
+        });
+    }),
+
+    http.get(`${apiBaseUrl}/fragile`, () => {
+        const rate = 1 / 10;
+        const random = Math.random();
+        if (random < rate) {
+            return HttpResponse.json({ message: 'Success!' });
+        } else {
+            return new HttpResponse(null, {
+                status: 500,
+                statusText: 'Internal Server Error',
+            });
+        }
+    }),
+);
